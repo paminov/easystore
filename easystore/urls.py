8903fdd768893599dc89debc02b9767a96cd6bf9
Parvizsho Aminov <p.aminov@yahoo.com>
2018-10-25 23:05:11 -0700

    adding google sign in and configuring redirect for logged in user

--- easystore/urls.py
+++ easystore/urls.py
@@ -5,10 +5,13 @@ from easystore import views
 
 urlpatterns = [
     path('', views.Home.as_view()),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html',
+                                           redirect_authenticated_user=True), 
+         name='login'),
     path('register/', views.Register.as_view(), name='register'),
     path('verify/', views.UserVerify.as_view()),
-    path('verify/<slug:username>/', views.UserVerify.as_view(), name='verify_user'),
+    path('verify/<slug:username>/', views.UserVerify.as_view(), 
+         name='verify_user'),
     path('api/upload', views.Upload.as_view()),
     path('api/contents', views.Contents.as_view()),
 ]
