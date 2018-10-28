58c9da6461d59b6f8713e3c6676cd99674f07755
Parvizsho Aminov <paminov@cisco.com>
2018-10-28 11:09:58 -0700

    adding logout view, user label and login/logout options

--- easystore/urls.py
+++ easystore/urls.py
@@ -2,12 +2,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from easystore import views
+from django_warrant.views import LogoutView
 
 urlpatterns = [
     path('', views.Home.as_view()),
     path('login/', auth_views.LoginView.as_view(template_name='login.html',
                                            redirect_authenticated_user=True), 
          name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'),
+         name='logout'),
     path('register/', views.Register.as_view(), name='register'),
     path('verify/', views.UserVerify.as_view()),
     path('verify/<slug:username>/', views.UserVerify.as_view(), 
